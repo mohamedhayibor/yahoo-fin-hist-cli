8c9c94db4739260c9e0d79cdd292540ca37aa688
Mohamed Hayibor <mohamedhayibor@gmail.com>
2017-08-27 06:07:01 -0400

    Add Impl for the remaining ranges

--- cli.js
+++ cli.js
@@ -25,7 +25,7 @@ const cli = meow(`
 let args = cli.input;
 
 /**
- * In case no argument exit immediately
+ * Case: no arguments exit immediately
  */
 if (args.length == 0) {
   console.log("Execute `fin -h` for commands");
@@ -34,21 +34,56 @@ if (args.length == 0) {
 
 let ticker = cli.input[0].trim().toUpperCase();
 
-/**
+/********************************************
  * Simplest use case: opening historical page
  * with no specifications.
  */
 if (args.length === 1) {
   open(`https://finance.yahoo.com/quote/${ ticker }/history`);
+  process.exit(1);
 }
 
+let date = cli.input[1].trim();
+let dateInMilli = Date.parse(date);
 
-let date1 = cli.input[1].trim();
+/*********************************************
+ * Case: opening page with only specified date
+ */
+if (Object.keys(cli.flags).length == 0) {
+  open(`https://finance.yahoo.com/quote/${ ticker }/history?period1=${ dateInMilli / 1000 }&period2=${ dateInMilli / 1000 }&interval=1d&filter=history&frequency=1d`);
+  process.exit(1);
+}
 
-let date1Mil = Date.parse(date1);
-// note: a day is 86400000 milliseconds == 86,400 seconds
-let date2Mil = date1Mil + 86400000;
+/*********************************************
+ * Case: opening page with a 5 day range
+ * note: a day is 86400000 milliseconds
+ */
+if (cli.flags.f) {
+  let start = (dateInMilli - (86400000 * 2)) / 1000;
+  let end = (dateInMilli + (86400000 * 2)) / 1000;
+  open(`https://finance.yahoo.com/quote/${ ticker }/history?period1=${ start }&period2=${ end }&interval=1d&filter=history&frequency=1d`);
 
-// open(`https://finance.yahoo.com/quote/${ ticker }/history?period1=${ date1Mil }`);
+  process.exit(1);
+}
 
-open(`https://finance.yahoo.com/quote/${ ticker }/history?period1=${ date1Mil / 1000 }&period2=${ date2Mil / 1000 }&interval=1d&filter=history&frequency=1d`);
+/*********************************************
+ * Case: opening page with a 10 day range
+ */
+if (cli.flags.t) {
+  let start = (dateInMilli - (86400000 * 5)) / 1000;
+  let end = (dateInMilli + (86400000 * 5)) / 1000;
+  open(`https://finance.yahoo.com/quote/${ ticker }/history?period1=${ start }&period2=${ end }&interval=1d&filter=history&frequency=1d`);
+
+  process.exit(1);
+}
+
+/*********************************************
+ * Case: opening page with a 30 day range
+ */
+if (cli.flags.m) {
+  let start = (dateInMilli - (86400000 * 15)) / 1000;
+  let end = (dateInMilli + (86400000 * 15)) / 1000;
+  open(`https://finance.yahoo.com/quote/${ ticker }/history?period1=${ start }&period2=${ end }&interval=1d&filter=history&frequency=1d`);
+
+  process.exit(1);
+}
